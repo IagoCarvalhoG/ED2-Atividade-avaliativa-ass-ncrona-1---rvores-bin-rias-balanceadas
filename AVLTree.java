4cc35c2c3680d2a21455adb1418745ca9bcc6837
IagoCarvalhoG <iago.00000845691@unicap.br>
2023-09-20 22:35:25 -0300

    zz

--- AVLTree.java
+++ AVLTree.java
@@ -5,8 +5,13 @@ import java.util.LinkedList;
 
 public class AVLTree<T extends Comparable <T>>{
     private AVLNode<T> root;
-    private LinkedList<Integer> rotationDirection = new LinkedList<Integer>();
-    private LinkedList<AVLNode<T>> rod = new LinkedList<AVLNode<T>>();
+    private boolean rotationInQueue;
+    private LinkedList<AVLNode<T>> nodes;
+
+    public AVLTree(){
+        this.rotationInQueue = false;
+        this.nodes = new LinkedList<AVLNode<T>>();
+    }
     
     public AVLNode<T> getRoot (){
         return this.root;
@@ -22,106 +27,174 @@ public class AVLTree<T extends Comparable <T>>{
         if(searchNode ==null){
             this.root = newNode;
         }else{
-            positionToPut(newNode,searchNode);
-        }
-         if(rod.size()>=3){
-            balancing(rotationDirection.pollFirst(), rotationDirection.pollFirst(), rod.pollFirst(), rod.pollFirst(), rod.pollFirst());
+            this.root = positionToPut(newNode,searchNode);
         }
     }
 
-    private void positionToPut (AVLNode<T> newNode, AVLNode<T> searchNode) {
+    private AVLNode<T> positionToPut (AVLNode<T> newNode, AVLNode<T> searchNode) {
         if(searchNode == null){
-            rod.addLast(newNode);
-            return;
+            return newNode;
         }
         if(searchNode.getInfo().compareTo(newNode.getInfo()) > 0){
             searchNode.setFatBal(searchNode.getFatBal()-1);
-            if(searchNode.getFatBal() >= 2 || searchNode.getFatBal() <= -2 || !rotationDirection.isEmpty()){
-                rod.addLast(searchNode);
-                rotationDirection.addLast(0);
+
+            if(searchNode.getFatBal() <= -2 && !this.rotationInQueue){
+                rotationInQueue = true;
+                positionToPut(newNode, searchNode.getLeft());
+                searchNode = rightRotation(searchNode);
             }
-            positionToPut(newNode, searchNode.getLeft());
+            
+            else{
+                 positionToPut(newNode, searchNode.getLeft());
             if(searchNode.getLeft() == null){
                 searchNode.setLeft(newNode);
              }
-            
+            }
+
         }else{
             searchNode.setFatBal(searchNode.getFatBal()+1);
-            if(searchNode.getFatBal() >= 2 || searchNode.getFatBal() <= -2|| !rotationDirection.isEmpty()){
-                rotationDirection.addLast(1);
-                rod.addLast(searchNode);
+            if(searchNode.getFatBal() >= 2 && !this.rotationInQueue){
+                rotationInQueue = true;
+                positionToPut(newNode, searchNode.getRight());
+                searchNode = leftRotation(searchNode);
             }
-             positionToPut(newNode, searchNode.getRight());
+            
+            else{
+            positionToPut(newNode, searchNode.getRight());
              if(searchNode.getRight() == null){
                  searchNode.setRight(newNode);
              }
+            }
         }
+        return searchNode;
     }
 
-    private void balancing(Integer value1, Integer value2, AVLNode<T> a, AVLNode<T> b, AVLNode<T>c){
-            a.setFatBal(0);
+    public void delete(T key){
+        searchNodeToDelete(key, this.root);
+        
+        AVLNode<T> nodeToDelete = nodes.pollFirst();
+        AVLNode<T> nodeToReplace = ne
+        if(nodeToDelete.getLeft() == null && nodeToDelete.getRight() == null){
+            nodeToDelete = null;
+        }
+        else if(nodeToDelete.getLeft() != null){
+            nodeToReplace = nodeToDelete.getLeft();
+            while(nodeToReplace.getRight() != null){
+            nodeToReplace = nodeToReplace.getRight();
+        }
+        }else{
+            nodeToReplace = nodeToDelete.getRight();
+        }
+        nodeToDelete.setInfo(nodeToReplace.getInfo());
+    }
+
+    private void searchNodeToDelete(T key, AVLNode<T> searchNode){
+         if(searchNode.getInfo() == key){
+            nodes.addFirst(searchNode);
+            return;
+        }
+          if(searchNode.getInfo().compareTo(key) > 0){
+            nodes.addFirst(searchNode);
+            searchNodeToDelete(key, searchNode.getLeft());
+            return;
+          }
+          
+          else{
+             nodes.addFirst(searchNode);
+            searchNodeToDelete(key, searchNode.getRight());
+            return;
+          }
+    }
+
+    private AVLNode<T> getHigherFromLeft(N){
+
+    }
+    private AVLNode<T> rightRotation(AVLNode<T>a){
+        AVLNode<T> b = a.getLeft();
+        AVLNode<T> c;
+        if(b.getFatBal() == -1 || (b.getFatBal() == -2 && a.getFatBal()==-2)){
+            a.setLeft(b.getRight());
+            b.setRight(a);
+            if(b.getFatBal() == -2){
+                a.setFatBal(+1);
+            }else{
+                a.setFatBal(0);
+            }
             b.setFatBal(0);
-            c.setFatBal(0);
-            AVLNode<T> rightLeaf = a.getRight();
-            AVLNode<T> leftLeaf = a.getLeft();
-            T aValue = a.getInfo();
-        //rotacao simples direita
-        if(value1 == 0 && value2 == 0){
-            AVLNode<T> t1 = c;
-            AVLNode<T> t2 = b.getRight();
-            AVLNode<T> t3 = a.getRight();
-            AVLNode<T>leftTree = new AVLNode<T>(t1.getInfo()); 
-            AVLNode<T>rightTree = new AVLNode<T>(aValue);
-            a.setInfo(b.getInfo());
-            rightLeaf.setInfo(aValue);
-            a.setLeft(t1);
-            rightLeaf.setLeft(t2);
-            rightLeaf.setLeft(t3);
+            a=b;
+        }else{
+            c = b.getRight();
+            b.setRight(c.getLeft());
+            a.setLeft(c.getRight());
+            c.setLeft(b);
+            c.setRight(a);
+            if(c.getFatBal() == 0){
+                b.setFatBal(0);
+                a.setFatBal(0);
+                c.setFatBal(0);
+            }else if(b.getFatBal() == 2 && c.getFatBal()==1){
+                a.setFatBal(0);
+                b.setFatBal(0);
+                c.setFatBal(1);
+            }else{
+                if(c.getFatBal() == -1){
+                b.setFatBal(b.getFatBal() -1);
+                a.setFatBal(1);
+                c.setFatBal(0);
+            }else{
+                a.setFatBal(0);
+                b.setFatBal(b.getFatBal() -2);
+                c.setFatBal(0);
+            }  
+            }
+            a=c;
         }
-        //rotacao dupla esquerda direita
-        else if(value1 == 0 && value2 ==1){
-            AVLNode<T> t1 = b.getLeft();
-            AVLNode<T> t2 = c.getLeft();
-            AVLNode<T> t3 = c.getRight();
-            AVLNode<T> t4 = a.getRight();
-            a.setInfo(c.getInfo());
-            rightLeaf.setInfo(aValue);
-            leftLeaf.setLeft(t1);
-            leftLeaf.setRight(t2);
-            rightLeaf.setLeft(t3);
-            rightLeaf.setRight(t4);
-        }
-        //rotacao dupla direita esquerda
-        else if(value1 == 1 && value2 == 0){
-            AVLNode<T> t1 = a.getLeft();
-            AVLNode<T> t2 = c.getLeft();
-            AVLNode<T> t3 = c.getRight();
-            AVLNode<T> t4 = b.getRight();
-            a.setInfo(c.getInfo());
-            leftLeaf.setInfo(aValue);
-            leftLeaf.setLeft(t1);
-            leftLeaf.setRight(t2);
-            rightLeaf.setLeft(t3);
-            rightLeaf.setRight(t4);
-        }
-        //rotacao simples esquerda
-        else if(value1 == 1 && value2 ==1){
-            AVLNode<T> t1 = a.getLeft();
-            AVLNode<T> t2 = b.getLeft();
-            AVLNode<T> t3 = c;
-            a.setInfo(b.getInfo());
-            leftLeaf.setInfo(aValue);
-            leftLeaf.setLeft(t1);
-            leftLeaf.setRight(t2);
-            rightLeaf = t3;
-
-        }
-        rod = null;
-        rotationDirection = null;
+        this.rotationInQueue = false;
+        return a;
     }
 
-    private void recalculateFatBal(T valueSearched){
-
+    private AVLNode<T> leftRotation(AVLNode<T>a){
+        AVLNode<T> b = a.getRight();
+        AVLNode<T> c;
+        if(b.getFatBal() == 1 || (b.getFatBal() == 2 && a.getFatBal()==2)){
+            a.setRight(b.getLeft());
+            b.setLeft(a);
+            if(b.getFatBal() == 2){
+                a.setFatBal(-1);
+            }else{
+                a.setFatBal(0);
+            }
+            b.setFatBal(0);
+            a=b;
+        }else{
+            c = b.getLeft();
+            b.setLeft(c.getRight());
+            a.setRight(c.getLeft());
+            c.setRight(b);
+            c.setLeft(a);
+            if(c.getFatBal() == 0){
+                b.setFatBal(0);
+                a.setFatBal(0);
+                c.setFatBal(0);
+            }else if(b.getFatBal() == -2 && c.getFatBal()==-1){
+                a.setFatBal(0);
+                b.setFatBal(0);
+                c.setFatBal(-1);
+            }else{
+                if(c.getFatBal() == 1){
+                b.setFatBal(b.getFatBal() +1);
+                a.setFatBal(-1);
+                c.setFatBal(0);
+            }else{
+                a.setFatBal(0);
+                b.setFatBal(b.getFatBal() +2);
+                c.setFatBal(0);
+            }  
+            }
+            a=c;
+        }
+        this.rotationInQueue = false;
+        return a;
     }
 
     public LinkedList<T> passeioEmOrdem(){
